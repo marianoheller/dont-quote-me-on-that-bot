fafa7211cc7af47c8a108eb2fac2a9b7978f9733
Mariano Mariano <marianofheller@gmail.com>
2018-02-07 10:20:21 -0300

    test setup

--- middleware/quoter.spec.js
+++ middleware/quoter.spec.js
@@ -1,1 +1,12 @@
 var quoter = require('./quoter');
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+
+
+
+describe("quoter testing", function() {
+    it("should be ok", (done) => {
+        done();
+    })
+} )
